b7f620bddd3f6d5b01b31c18a41c93b6e2cbf85f
rafafariac <rafaellafaria135@gmail.com>
2024-08-07 19:04:24 -0300

    funcionalidades

---
+++ pydaq/guis/ui_PyDAQ_get_model_Arduino_Results_widget.py
@@ -0,0 +1,318 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'PyDAQ_get_model_Arduino_Results_widgetmXRqtd.ui'
+##
+## Created by: Qt User Interface Compiler version 6.6.0
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (
+    QCoreApplication,
+    QDate,
+    QDateTime,
+    QLocale,
+    QMetaObject,
+    QObject,
+    QPoint,
+    QRect,
+    QSize,
+    QTime,
+    QUrl,
+    Qt,
+)
+from PySide6.QtGui import (
+    QBrush,
+    QColor,
+    QConicalGradient,
+    QCursor,
+    QFont,
+    QFontDatabase,
+    QGradient,
+    QIcon,
+    QImage,
+    QKeySequence,
+    QLinearGradient,
+    QPainter,
+    QPalette,
+    QPixmap,
+    QRadialGradient,
+    QTransform,
+)
+from PySide6.QtWidgets import QApplication, QGridLayout, QLabel, QSizePolicy, QWidget
+
+
+class Ui_show_results(object):
+    def setupUi(self, show_results):
+        if not show_results.objectName():
+            show_results.setObjectName("show_results")
+        show_results.resize(318, 294)
+        sizePolicy = QSizePolicy(QSizePolicy.Ignored, QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(show_results.sizePolicy().hasHeightForWidth())
+        show_results.setSizePolicy(sizePolicy)
+        show_results.setMinimumSize(QSize(0, 0))
+        show_results.setMaximumSize(QSize(16777215, 16777215))
+        show_results.setStyleSheet(
+            "QComboBox QAbstractItemView {\n"
+            "    background-color: rgb(77, 77, 77);\n"
+            "}\n"
+            "\n"
+            "QComboBox QAbstractItemView::item:focus{\n"
+            "    background-color: rgb(140, 140, 140);\n"
+            "}\n"
+            "\n"
+            "QDoubleSpinBox{\n"
+            "	background-color: rgb(77, 77, 77);\n"
+            "	\n"
+            "	border-top: 1.5px solid rgb(46, 46, 46);\n"
+            "	border-left: 1.5px solid rgb(46, 46, 46);\n"
+            "\n"
+            "	border-bottom: 1.5px solid rgb(166, 166, 166);\n"
+            "	border-right: 1.5px solid rgb(166, 166, 166);\n"
+            "}\n"
+            "\n"
+            "QDoubleSpinBox::up-button{\n"
+            "    image: url(:/imgs/imgs/drop_up_arrow.png);\n"
+            "	width: 11px;\n"
+            "\n"
+            "	background-color: rgb(0, 79, 0);\n"
+            "	border-top: 1.5px solid rgb(127, 167, 127);\n"
+            "	border-left: 1.5px solid rgb(127, 167, 127);\n"
+            "\n"
+            "	border-bottom: 1.5px solid rgb(0, 0, 0);\n"
+            "	border-right: 1.5px solid rgb(0, 0, 0);\n"
+            "}\n"
+            "\n"
+            "QDoubleSpinBox::up-button:pressed{\n"
+            "	border: 2px solid rgb(255, 255, 255);\n"
+            "}\n"
+            "\n"
+            "QDoubleSpinBox::up-button:hover{\n"
+            "	background-color: rgb(0, 50, 0);\n"
+            "}\n"
+            "\n"
+            "QDoubleSpinBox::down-button{\n"
+            ""
+            "    image: url(:/imgs/imgs/drop_down_arrow.png);\n"
+            "	width: 11px;\n"
+            "\n"
+            "	background-color: rgb(0, 79, 0);\n"
+            "	border-top: 1.5px solid rgb(127, 167, 127);\n"
+            "	border-left: 1.5px solid rgb(127, 167, 127);\n"
+            "\n"
+            "	border-bottom: 1.5px solid rgb(0, 0, 0);\n"
+            "	border-right: 1.5px solid rgb(0, 0, 0);\n"
+            "}\n"
+            "\n"
+            "QDoubleSpinBox::down-button:pressed{\n"
+            "	border: 2px solid rgb(255, 255, 255);\n"
+            "}\n"
+            "\n"
+            "QDoubleSpinBox::down-button:hover{\n"
+            "	background-color: rgb(0, 50, 0);\n"
+            "}\n"
+            "\n"
+            "QSpinBox{\n"
+            "	background-color: rgb(77, 77, 77);\n"
+            "	\n"
+            "	border-top: 1.5px solid rgb(46, 46, 46);\n"
+            "	border-left: 1.5px solid rgb(46, 46, 46);\n"
+            "\n"
+            "	border-bottom: 1.5px solid rgb(166, 166, 166);\n"
+            "	border-right: 1.5px solid rgb(166, 166, 166);\n"
+            "}\n"
+            "\n"
+            "QSpinBox::up-button{\n"
+            "    image: url(:/imgs/imgs/drop_up_arrow.png);\n"
+            "	width: 11px;\n"
+            "\n"
+            "	background-color: rgb(0, 79, 0);\n"
+            "	border-top: 1.5px solid rgb(127, 167, 127);\n"
+            "	border-left: 1.5px solid rgb(127, 167, 127);\n"
+            "\n"
+            "	border-b"
+            "ottom: 1.5px solid rgb(0, 0, 0);\n"
+            "	border-right: 1.5px solid rgb(0, 0, 0);\n"
+            "}\n"
+            "\n"
+            "QSpinBox::up-button:pressed{\n"
+            "	border: 2px solid rgb(255, 255, 255);\n"
+            "}\n"
+            "\n"
+            "QSpinBox::up-button:hover{\n"
+            "	background-color: rgb(0, 50, 0);\n"
+            "}\n"
+            "\n"
+            "QSpinBox::down-button{\n"
+            "    image: url(:/imgs/imgs/drop_down_arrow.png);\n"
+            "	width: 11px;\n"
+            "\n"
+            "	background-color: rgb(0, 79, 0);\n"
+            "	border-top: 1.5px solid rgb(127, 167, 127);\n"
+            "	border-left: 1.5px solid rgb(127, 167, 127);\n"
+            "\n"
+            "	border-bottom: 1.5px solid rgb(0, 0, 0);\n"
+            "	border-right: 1.5px solid rgb(0, 0, 0);\n"
+            "}\n"
+            "\n"
+            "QSpinBox::down-button:pressed{\n"
+            "	border: 2px solid rgb(255, 255, 255);\n"
+            "}\n"
+            "\n"
+            "QSpinBox::down-button:hover{\n"
+            "	background-color: rgb(0, 50, 0);\n"
+            "}\n"
+            "\n"
+            "QWidget#PRBS_Info{\n"
+            "	background-color: rgb(64, 64, 64);\n"
+            "}\n"
+            "\n"
+            "QWidget{\n"
+            "	color: rgb(255, 255, 255);\n"
+            "}\n"
+            "\n"
+            "QComboBox{\n"
+            "	background-color: rgb(77, 77, 77);\n"
+            "	\n"
+            "	border-top: 1.5px solid rgb(46, 46, 46);\n"
+            "	border-le"
+            "ft: 1.5px solid rgb(46, 46, 46);\n"
+            "\n"
+            "	border-bottom: 1.5px solid rgb(166, 166, 166);\n"
+            "	border-right: 1.5px solid rgb(166, 166, 166);\n"
+            "}\n"
+            "\n"
+            "\n"
+            "QComboBox::drop-down{\n"
+            "	image: url(:/imgs/imgs/drop_down_arrow.png);\n"
+            "	width: 11px;\n"
+            "\n"
+            "	background-color: rgb(0, 79, 0);\n"
+            "	border-top: 2px solid rgb(127, 167, 127);\n"
+            "	border-left: 2px solid rgb(127, 167, 127);\n"
+            "\n"
+            "	border-bottom: 2px solid rgb(0, 0, 0);\n"
+            "	border-right: 2px solid rgb(0, 0, 0);\n"
+            "}\n"
+            "\n"
+            "QComboBox::drop-down:hover{\n"
+            "	background-color: rgb(0, 50, 0);\n"
+            "}\n"
+            "\n"
+            "QComboBox::drop-down:pressed{\n"
+            "	border: 2px solid rgb(255, 255, 255);\n"
+            "}\n"
+            "\n"
+            "QPushButton{\n"
+            "	background-color: rgb(0, 79, 0);\n"
+            "\n"
+            "	border-top: 1.5px solid rgb(127, 167, 127);\n"
+            "	border-left: 1.5px solid rgb(127, 167, 127);\n"
+            "\n"
+            "	border-bottom: 1.5px solid rgb(0, 0, 0);\n"
+            "	border-right: 1.5px solid rgb(0, 0, 0);\n"
+            "\n"
+            "	\n"
+            '	font: 12pt "Helvetica";\n'
+            "	text-align:center;\n"
+            "}\n"
+            "\n"
+            "QWidget{\n"
+            '	font: 12pt "H'
+            'elvetica";\n'
+            "}\n"
+            "\n"
+            "QPushButton:hover{\n"
+            "	background-color: rgb(0, 50, 0);\n"
+            "}\n"
+            "\n"
+            "QPushButton:pressed{\n"
+            "	border: 2px solid rgb(255, 255, 255);\n"
+            "}\n"
+            "\n"
+            "QLineEdit{\n"
+            "	background-color: rgb(77, 77, 77);\n"
+            "	border-top: 1.5px solid rgb(46, 46, 46);\n"
+            "	border-left: 1.5px solid rgb(46, 46, 46);\n"
+            "\n"
+            "	border-bottom: 1.5px solid rgb(166, 166, 166);\n"
+            "	border-right: 1.5px solid rgb(166, 166, 166);\n"
+            "}\n"
+            "\n"
+            "QRadioButton::indicator{\n"
+            "	border-radius: 6px;\n"
+            "	border-top: 1.5px solid rgb(0, 0, 0);\n"
+            "	border-left: 1.5px solid rgb(0, 0, 0);\n"
+            "\n"
+            "	border-bottom: 1.5px solid rgb(160, 160, 160);\n"
+            "	border-right: 1.5px solid rgb(160, 160, 160);\n"
+            "}\n"
+            "\n"
+            "QRadioButton::indicator::checked{\n"
+            "	background-color: white;\n"
+            "}\n"
+            "\n"
+            "QRadioButton::indicator::unchecked:hover{\n"
+            "	background-color: #9F9F9F;\n"
+            "}\n"
+            "\n"
+            "QRadioButton::indicator::pressed{\n"
+            "	border: 1.5px solid #505050\n"
+            "}\n"
+            "\n"
+            "QPushButton#reload_devices{\n"
+            "	image: url(:/imgs/imgs/reload.png);\n"
+            ""
+            "	width: 11px;\n"
+            "	background-color: rgb(0, 79, 0);\n"
+            "\n"
+            "	border-top: 1.5px solid rgb(127, 167, 127);\n"
+            "	border-left: 1.5px solid rgb(127, 167, 127);\n"
+            "\n"
+            "	border-bottom: 1.5px solid rgb(0, 0, 0);\n"
+            "	border-right: 1.5px solid rgb(0, 0, 0);\n"
+            "\n"
+            "	\n"
+            '	font: 12pt "Helvetica";\n'
+            "	text-align:center;\n"
+            "}\n"
+            "\n"
+            "QPushButton#reload_devices:hover{\n"
+            "	background-color: rgb(0, 50, 0);\n"
+            "}\n"
+            "\n"
+            "QPushButton#reload_devices:pressed{\n"
+            "	border: 2px solid rgb(255, 255, 255);\n"
+            "}"
+        )
+        self.gridLayout = QGridLayout(show_results)
+        self.gridLayout.setObjectName("gridLayout")
+        self.widget = QWidget(show_results)
+        self.widget.setObjectName("widget")
+        self.gridLayout_2 = QGridLayout(self.widget)
+        self.gridLayout_2.setObjectName("gridLayout_2")
+        self.label_2 = QLabel(self.widget)
+        self.label_2.setObjectName("label_2")
+
+        self.gridLayout_2.addWidget(self.label_2, 0, 0, 1, 1)
+
+        self.gridLayout.addWidget(self.widget, 1, 0, 1, 1)
+
+        self.retranslateUi(show_results)
+
+        QMetaObject.connectSlotsByName(show_results)
+
+    # setupUi
+
+    def retranslateUi(self, show_results):
+        show_results.setWindowTitle(
+            QCoreApplication.translate(
+                "show_results", "Model Acquisition - Results", None
+            )
+        )
+        self.label_2.setText(QCoreApplication.translate("show_results", "Text", None))
+
+    # retranslateUi
